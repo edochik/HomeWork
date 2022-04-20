abe4bc64612b411bff629cded8d2073ec27ac017
edochik <edoch@yandex.ru>
2022-04-20 20:42:23 +0300

    modified Task6

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -1,26 +1,24 @@
-﻿int n = 918;
+﻿// изначально считал что нужно до сотых, но можно кол-во вывести до 10 без сотых 
+// и будет выводится последняя цифра.
+int n = 716;
 int i1 = 100;
 int i2 = 10;
 int result = 0;
 int result1 = 0;
-int result2 = 0;
-int result3 = 0;
 
 while (i1 < n)
 {
     i1 = i1 + 100;
     result = result + 100;
-    result1 = result - 100;
 }
 int a = n - result;
 
 while (i2 < a)
 {
     i2 = i2 + 10;
-    result2 = result2 + 10;
-    result3 = result3 - 10;
+    result1 = result1 + 10;
 
 }
-int b = a - result2;
+int b = a - result1;
 Console.WriteLine(b);
 
